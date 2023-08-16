64524abf43c94a0070d86b4150ef2f0423b42a69
Alfonso Fuentes <fuentesw3008@gmail.com>
2023-08-16 17:35:51 -0500

    finalGifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,22 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { HomePageComponent } from './pages/home/home-page.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
+    CardComponent,
+    CardListComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [
-    HomePageComponent
+    HomePageComponent,
   ]
 })
 export class GifsModule { }
